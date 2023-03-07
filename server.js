e0b54594d53f7df0f8237e4120a1ad3e010d7527
Arpan Sharma <arpan.sharma26@gmail.com>
2023-03-06 19:47:55 -0600

    backend code execution

--- server.js
+++ server.js
@@ -13,30 +13,45 @@ app.post("/", async (req, res) => {
         // host: "smtp.coby.ns.cloudflare.com",
         // port: 2525,
         // secure: true, //true for 465, false for other ports
+        // euehqepiirawueim
         service: "Gmail",
         logger: true,
         debug: true,
         auth: {
           user: "welcometeam@erinskyekelly.com", // generated ethereal user
           pass: "euehqepiirawueim", // generated ethereal password
+
+          // user: "welcometeam@erinskyekelly.com", // generated ethereal user
+          // pass: "euehqepiirawueim", // generated ethereal password
         },
-        // tls: {
-        //   // do not fail on invalid certs
-        //   rejectUnauthorized: false,
-        // },
+        tls: {
+          // do not fail on invalid certs
+          rejectUnauthorized: false,
+        },
       });
 
       let details = {
+        headers: {
+          "x-priority": "1",
+          "x-msmail-priority": "High",
+          importance: "high"
+      },
         from: 'welcometeam@erinskyekelly.com', // sender address
+        // from: 'welcometeam@erinskyekelly.com', // sender address
         to: req.body.email, // list of receivers
-        subject: "Test Email - Quiz Link", // Subject line
-        text: `     Hi,
-                    Your partner invited you to attempt this quiz - bit.ly/3ERI4Sw
+        subject: "Your partner wants you to take the Naked Money Meetings online quiz", // Subject line
+        // text: `     Hi,
+        //             Your partner invited you to attempt this quiz - bit.ly/3ERI4Sw
 
 
-                    Thank you!
-        `, // plain text body
-        // html: "<b>Hello world?</b>", // html body
+        //             Thank you!
+        // `, // plain text body
+
+  //       Your partner, _______, has sent you this email so you can discover your Money Block and find out the naked truth about why you are unconsciously sabotaging your ability to build wealth. 
+	// CLICK HERE to get started. 
+
+        html: `<p>Your partner, <em><b>${req.body.firstname}</b></em><br/> <br/>
+                                     has sent you this email so you can discover your Money Block and find out the naked truth about why you are unconsciously sabotaging your ability to build wealth. <a href='http://ec2-15-223-72-54.ca-central-1.compute.amazonaws.com:3000/'>CLICK HERE</a> to get started. </p>`, // html body
       };
 
       transporter.sendMail(details, (err) => {
